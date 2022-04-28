b4da2cef168f0e641f384d251e9ba5cec4e430fa
TheMires <thamiresfreitas051@gmail.com>
2022-04-28 15:02:48 -0300

    @TheMires [Task: Criação de Tabelas com ORM EF] {28/04/2022}

---
+++ BlogPessoal/src/model/PostModel.cs
@@ -0,0 +1,30 @@
+﻿using BlogPessoal.src.modelos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogPessoal.src.model
+{
+    [Table("tb_posts")]
+    public class PostModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public string Photo { get; set; }
+
+        [ForeignKey("fk_user")]
+        public UserModel Creator { get; set; }
+
+        [ForeignKey("fk_theme")]
+        public ThemeModel Theme { get; set; }
+    }
+}
---
+++ BlogPessoal/src/model/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using BlogPessoal.src.model;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace BlogPessoal.src.modelos
+{
+    [Table("tb_user")]
+    public class UserModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(30)]
+        public string Email { get; set; }
+
+        [Required, StringLength(30)]
+        public string Password { get; set; }
+
+        public string Photo { get; set; }
+
+        [JsonIgnore]
+        public List<PostModel> MyPosts { get; set; }
+    }
+}
