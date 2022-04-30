092f3e7619216788a811e75ce19145c129fb7a50
TheMires <thamiresfreitas051@gmail.com>
2022-04-29 22:29:27 -0300

    @TheMires [Task: Criando DTOs e repositorio] {29/04/2022}

---
+++ BlogPessoal/src/repositorios/ITema.cs
@@ -0,0 +1,19 @@
+﻿using BlogPessoal.src.model;
+using System.Collections.Generic;
+
+namespace BlogPessoal.src.repositorios
+{/// <summary>
+ /// <para>Resumo: Responsavel por representar ações de CRUD de tema</para>
+ /// <para>Criado por: Thamires Freitas</para>
+ /// <para>Versão: 1.0</para>
+ /// <para>Data: 29/04/2022</para>
+ /// </summary>
+    public interface ITema
+    {
+        void NovoTema(NovoTemaDTO tema);
+        void AtualizarTema(AtualizarTemaDTO tema);
+        void DeletarTema(int id);
+        TemaModelo PegarTemaPeloId(int id);
+        List<TemaModelo> PegarTemaPelaDescricao(string descricao);
+    }
+}
